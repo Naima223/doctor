4d735be09c9c453f7d29cae46e68c09520351336
fairuz-anadi <fairuz.cse.20230104121@aust.edu>
2025-08-22 16:37:42 +0600

    update: vercel added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Doctors from './pages/Doctors'
-import Login from './pages/Login'
 import Footer from './components/Footer'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -15,9 +14,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/doctors' element={<Doctors />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
+<Route path="/doctors" element={<Doctors />} />      </Routes>
       <Footer />
     </div>
   )
