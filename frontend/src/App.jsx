397130ded390902b7b16530dda51f4a23beee360
easteak00987 <easteak.cse.20230104123@aust.edu>
2025-08-24 00:47:21 +0600

    Updated contact page with new address, phone, email, and QuickDoc careers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,26 +2,24 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-
 import Doctors from './pages/Doctors'
-
 import Login from './pages/Login'
-
+import Contact from './pages/Contact'
 import Footer from './components/Footer'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
       <ToastContainer />
       <Navbar />
+
       <Routes>
         <Route path='/' element={<Home />} />
-
-<Route path="/doctors" element={<Doctors />} />      </Routes>
-
-         <Route path='/login' element={<Login />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
 
       <Footer />
